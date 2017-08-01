20c2dba33bd1531a3c8236338da24c2561f7fdd2
卞泽鑫 <junjiebest8@gmail.com>
2017-08-01 15:03:24 +0800

    add statementcallback (reverted from commit 46bcce9a318372333b269f4c5f36aa99b0d5840c)

--- src/main/java/com/getui/bi/spring/boot/quickstart/MainTest.java
+++ src/main/java/com/getui/bi/spring/boot/quickstart/MainTest.java
@@ -5,121 +5,37 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = HiveDataSource.class)
-public class MainTest {
+@SpringBootTest(classes=HiveDataSource.class)
+public class MainTest {  
+      
+    @Autowired  
+    @Qualifier("hiveJdbcTemplate")  
+    JdbcTemplate hiveJdbcTemplate;  
+      
+    @Test    
+    public void DataSourceTest() {   
+    	
+//    	StringBuffer sql = new StringBuffer("show tables");
+          
+        // create table  
+        StringBuffer sql = new StringBuffer("create table IF NOT EXISTS ");  
+        sql.append("HIVE_TEST1 ");  
+        sql.append("(KEY INT, VALUE STRING) ");  
+        sql.append("PARTITIONED BY (S_TIME DATE)"); // 分区存储  
+        sql.append("ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' "); // 定义分隔符  
+        sql.append("STORED AS TEXTFILE"); // 作为文本存储  
+          
+        // drop table  
+//      StringBuffer sql = new StringBuffer("DROP TABLE IF EXISTS ");  
+//      sql.append("HIVE_TEST1");  
+          
+          
+        hiveJdbcTemplate.execute(sql.toString());  
+    }   
 
-    @Autowired
-    @Qualifier("hiveJdbcTemplate")
-    JdbcTemplate hiveJdbcTemplate;
-
-    @Test
-    public void DataSourceTest() {
-
-        StringBuffer sql = new StringBuffer("select * from book limit 5");
-
-        // create table
-//        StringBuffer sql = new StringBuffer("create table IF NOT EXISTS ");
-//        sql.append("HIVE_TEST1 ");
-//        sql.append("(KEY INT, VALUE STRING) ");
-//        sql.append("PARTITIONED BY (S_TIME DATE)"); // 分区存储
-//        sql.append("ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' "); // 定义分隔符
-//        sql.append("STORED AS TEXTFILE"); // 作为文本存储
-
-        // drop table
-//        StringBuffer sql = new StringBuffer("DROP TABLE IF EXISTS ");
-//        sql.append("HIVE_TEST1");
-
-
-//        hiveJdbcTemplate.execute(sql.toString());
-        hiveJdbcTemplate.execute(sql.toString(), new ArticleListPreparedStatement());
-    }
-
-    class ArticleListPreparedStatement implements PreparedStatementCallback<List<Book>> {
-        public List<Book> doInPreparedStatement(PreparedStatement ps)
-                throws SQLException, DataAccessException {
-            return QueryUtils.extractArticleListFromRs(ps.executeQuery());
-        }
-    }
-
-    static class QueryUtils {
-        public static Book extractArticleFromRs(ResultSet rs) throws SQLException {
-            Book article = new Book();
-            article.setId(rs.getInt("id"));
-            article.setName(rs.getString("name"));
-            article.setAge(rs.getInt("age"));
-            article.setPubdate(rs.getString("pubdate"));
-            System.out.println(article.toString());
-            return article;
-        }
-
-        public static List<Book> extractArticleListFromRs(ResultSet rs) throws SQLException {
-            List<Book> articleList = new ArrayList<Book>();
-            while (rs.next()) {
-                articleList.add(extractArticleFromRs(rs));
-            }
-            return articleList;
-        }
-    }
-
-    static class Book {
-        private int id;
-        private String name;
-        private int age;
-        private String pubdate;
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public String getPubdate() {
-            return pubdate;
-        }
-
-        public void setPubdate(String pubdate) {
-            this.pubdate = pubdate;
-        }
-
-        @Override
-        public String toString() {
-            return "Book{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    ", age=" + age +
-                    ", pubdate='" + pubdate + '\'' +
-                    '}';
-        }
-    }
-
+    
 }
