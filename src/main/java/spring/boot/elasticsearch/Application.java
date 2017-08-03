baa386d58c4bbca41679fc4f3c54fddb2df17aa1
卞泽鑫 <junjiebest8@gmail.com>
2017-08-03 16:18:11 +0800

    spring boot integration with elasticsearch

--- src/main/java/spring/boot/elasticsearch/Application.java
+++ src/main/java/spring/boot/elasticsearch/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -15,6 +17,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAutoConfiguration
 @ComponentScan
 @EnableScheduling
+@EnableMongoRepositories
+@EnableJpaRepositories
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
